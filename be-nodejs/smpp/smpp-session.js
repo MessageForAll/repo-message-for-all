9dea999abe3b80b876f92ae68e0b54cafc9c7f39
“Elton <elton.braz@hotmail.com>
2019-02-16 22:55:25 -0200

    criado pasta smpp em be-nodejs

---
+++ be-nodejs/smpp/smpp-session.js
@@ -0,0 +1,41 @@
+"use strict";
+var smpp = require("smppnew");
+const inputData = {
+  URL: "smpp://10.125.124.24:17600",
+  YOUR_SYSTEM_ID: "oemtstipa",
+  YOUR_PASSWORD: "oemrtz",
+  DESTINATION_NUMBER: "5521994309366",
+  SHORT_MESSAGE_TEXT: "Hello!"
+};
+console.log("oi");
+var session = smpp.connect(inputData.URL);
+
+session.bind_transceiver(
+  {
+    system_id: inputData.YOUR_SYSTEM_ID,
+    password: inputData.YOUR_PASSWORD
+  },
+  function(pdu) {
+    if (pdu.command_status == 0) {
+      // Successfully bound
+      session.submit_sm(
+        {
+          destination_addr: inputData.DESTINATION_NUMBER,
+          short_message: inputData.SHORT_MESSAGE_TEXT
+        },
+        function(pdu) {
+          if (pdu.command_status == 0) {
+            // Message successfully sent
+            console.log(pdu.message_id);
+          } else {
+            console.log(pdu.command_status);
+          }
+        }
+      );
+    } else {
+      console.log(pdu.command_status);
+    }
+  }
+);
+
+console.log("tchau");
